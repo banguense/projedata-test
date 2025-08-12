e8e31585e7baec1c067eedbf982a4c63cc601ec8
Hector Fernandes <hectorhu17@gmail.com>
2025-08-12 15:48:17 -0300

    feat: create intial entities

---
+++ src/main/java/br/dev/hector/Entities/Person.java
@@ -0,0 +1,22 @@
+package br.dev.hector.Entities;
+
+import java.time.LocalDate;
+
+public class Person {
+
+  private String name;
+  private LocalDate dateOfBirth;
+
+  public Person(String name, LocalDate dateOfBirth) {
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return dateOfBirth;
+  }
+}
---
+++ src/main/java/br/dev/hector/Principal.java
@@ -0,0 +1,7 @@
+package br.dev.hector;
+
+public class Principal {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
