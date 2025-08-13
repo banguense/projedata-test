c8b7109dcf331c1fdf20c5721967335939c2d048
Hector Fernandes <hectorhu17@gmail.com>
2025-08-12 22:01:05 -0300

    feat: create a main method

--- src/main/java/br/dev/hector/Principal.java
+++ src/main/java/br/dev/hector/Principal.java
@@ -1,7 +1,29 @@
 package br.dev.hector;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.dev.hector.Entities.Employee;
+import br.dev.hector.Entities.Employees;
+
 public class Principal {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    List<Employee> listEmployee = new ArrayList<>(List.of(
+        new Employee("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"),
+        new Employee("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"),
+        new Employee("Caio", LocalDate.of(1961, 5, 2), new BigDecimal("9836.14"), "Coordenador"),
+        new Employee("Miguel", LocalDate.of(1988, 10, 14), new BigDecimal("19119.88"), "Diretor"),
+        new Employee("Alice", LocalDate.of(1995, 1, 5), new BigDecimal("2234.68"), "Recepcionista"),
+        new Employee("Heitor", LocalDate.of(1999, 11, 19), new BigDecimal("1582.72"), "Operador"),
+        new Employee("Arthur", LocalDate.of(1993, 3, 31), new BigDecimal("4071.84"), "Contador"),
+        new Employee("Laura", LocalDate.of(1994, 7, 8), new BigDecimal("3017.45"), "Gerente"),
+        new Employee("Heloísa", LocalDate.of(2003, 5, 24), new BigDecimal("1606.85"), "Eletricista"),
+        new Employee("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente")));
+    Employees employees = new Employees(listEmployee);
+
+    employees.removeEmployee("João");
+    employees.showEmployees();
   }
 }
